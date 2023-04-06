db0f92cc20644f9e6ba933470a9c93e0fdab6c24
mogakamo <mogaka.amo254@gmail.com>
2023-04-06 23:27:50 +0300

    [feat]: working on the bottom navigation

---
+++ src/stack/BottonmStack.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+ 
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function BottomStack() {
+  return (
+    <View>
+      <Text className="text-green-500">BottomStack</Text>
+    </View>
+  );
+}
