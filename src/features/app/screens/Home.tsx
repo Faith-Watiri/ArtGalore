bc263eff1a0a711bd29657d314087b1824d81314
mogakamo <mogaka.amo254@gmail.com>
2023-04-06 23:25:36 +0300

    [commit]: setting up native navigation

---
+++ src/features/app/screens/Home.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export function HomeScreen() {
+  return (
+    <View className="flex-1 bg-white">
+      <Text className="text-green-500">HomeScreen</Text>
+    </View>
+  );
+}
