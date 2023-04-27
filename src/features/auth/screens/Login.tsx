355011fb9bb5e99a7c66614d7ae4fd635431f397
Faith-Watiri <watiri216@gmail.com>
2023-04-27 14:43:09 +0300

    [commit]: log in and registration request done

--- src/features/auth/screens/Login.tsx
+++ src/features/auth/screens/Login.tsx
@@ -1,32 +1,65 @@
 /* eslint-disable prettier/prettier */
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, ToastAndroid } from 'react-native';
+import React, {useState} from 'react';
 import { FormInput, PrimaryButton } from '../../../components';
 import { useNavigation } from '@react-navigation/native';
 import { AuthLayout } from '../components';
+import { BASE_URL } from '../../../lib/constants';
+import { axiosReq } from '../../../utlis/axios';
 
 export function Login() {
   const navigation = useNavigation()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const[data, setData] = useState([])
+
+  const config: AxiosConfig = {
+    url: `${BASE_URL}/auth/login`,
+    method: 'POST',
+    data: data
+  }
+
+  const onSubmit = async() => {
+    setData({...data, email, password})
+
+    const login = await axiosReq(config)
+
+    console.log(login)
+
+    if (login.status === 201) {
+      navigation.navigate("Register")
+
+      ToastAndroid.showWithGravityAndOffset(
+        'Logged in successfully!',
+        ToastAndroid.BOTTOM,
+        ToastAndroid.LONG,
+        25,
+        50,
+    )
+    }
+
+  }
+
   return (
     <AuthLayout 
         pageTitle='Login'
         accountCheck='Dont have an account?'
         screen='Register'
         action='Login' 
-        onPress={() => {}}>
+        onPress={onSubmit}>
       <View>
         <FormInput
           label='Email Address'
-          value=''
+          value={email}
           placeholder='johndoe@gmail.com'
-          onChangeText={() => { }}
+          onChangeText={(text) => setEmail(text)}
           secureTextEntry={false}
         />
         <FormInput
           label='Password'
-          value=''
+          value={password}
           placeholder='johndoe@gmail.com'
-          onChangeText={() => { }}
+          onChangeText={(text) => setPassword(text)}
           secureTextEntry={true}
         />
       </View>
--- src/features/auth/screens/Register.tsx
+++ src/features/auth/screens/Register.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ToastAndroid } from 'react-native';
 import { AuthLayout } from '../components';
 import { FormInput, RadioCheckButton } from '../../../components';
 import { BASE_URL } from '../../../lib/constants';
 import { axiosReq } from '../../../utlis/axios';
+import { useNavigation } from '@react-navigation/native';
 
 export function Register() {
     const [name, setName] = useState('')
@@ -14,22 +15,41 @@ export function Register() {
     const [confirmPassword, setConfirmPassword] = useState('')
     const [data, setData] = useState([])
 
+    const navigation = useNavigation()
+
     const handlePress = (value: string) => {
         setRole(value)
     }
-    
+
     const config: AxiosConfig = {
         url: `${BASE_URL}/auth/register`,
         method: 'POST',
         data: data
     }
-    
+
     const onSubmit = async () => {
         setData({ ...data, name, email, password, role });
-        
+
         const res = await axiosReq(config)
 
-        console.log(res.data)
+        if (res.status === 201) {
+            navigation.navigate('Login')
+            ToastAndroid.showWithGravityAndOffset(
+                'User created successfully!',
+                ToastAndroid.BOTTOM,
+                ToastAndroid.LONG,
+                25,
+                50,
+            )
+        } else {
+            ToastAndroid.showWithGravityAndOffset(
+                `${res.status}`,
+                ToastAndroid.BOTTOM,
+                ToastAndroid.LONG,
+                25,
+                50,
+            )
+        }
     }
 
 
